68244a5a9b54f62a73cfd7b6b8ccc7560c6c3ab6
nobuhiro <nobuhiro>
2009-07-06 02:27:05 +0000

    Remove unused script.

--- build_rootfs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/tcsh
-tar zxf ramdisk.tgz >& rootfs_build.log
-cp ../busybox1/busybox ./disk/bin/ >>& rootfs_build.log
-./e2fsimage -v -f ramdisk.img -d disk -s 8192 >>& rootfs_build.log
-cp ramdisk.img ../../ >>& rootfs_build.log
