21978b925b1b9f9069392bb75b3c1ec3c62ad0b7
Cynthia Huang <cynthia@andestech.com>
2015-10-19 16:39:11 +0800

    Remove .tar file, and add disk folder.

---
+++ disk/etc/init.d/run.sh
@@ -0,0 +1,113 @@
+#!/bin/sh
+export hostname0=
+export mnt_path0=
+export hostname1=
+export mnt_path1=
+export auto=
+dmesg > dmesg_boot.log
+drvs="FTGPIO010 faraday-rtc FTMAC100 ftsdc010 ADS7846 40x30 ftssp010"
+test -e lib/modules/3.4.35+/kernel/drivers/gpio/gpio-ftgpio010.ko && \
+{
+	insmod lib/modules/3.4.35+/kernel/drivers/gpio/gpio-ftgpio010.ko
+	if [ "$auto" == "1" ]; then
+		rmmod gpio-ftgpio010
+		insmod lib/modules/3.4.35+/kernel/drivers/gpio/gpio-ftgpio010.ko
+	fi	
+}
+test -e lib/modules/3.4.35+/kernel/drivers/net/ethernet/faraday/ftmac100.ko && \
+{
+	insmod lib/modules/3.4.35+/kernel/drivers/net/ethernet/faraday/ftmac100.ko
+	if [ "$auto" == "1" ]; then
+		rmmod ftmac100
+		insmod lib/modules/3.4.35+/kernel/drivers/net/ethernet/faraday/ftmac100.ko
+	fi
+}
+test -e lib/modules/3.4.35+/kernel/drivers/mmc/host/ftsdc010.ko && \
+{
+	insmod lib/modules/3.4.35+/kernel/drivers/mmc/core/mmc_core.ko
+	insmod lib/modules/3.4.35+/kernel/drivers/mmc/card/mmc_block.ko
+	insmod lib/modules/3.4.35+/kernel/drivers/mmc/host/ftsdc010.ko
+	if [ "$auto" == "1" ]; then
+		rmmod ftsdc010
+		rmmod mmc_block
+		rmmod mmc_core
+		insmod lib/modules/3.4.35+/kernel/drivers/mmc/core/mmc_core.ko
+		insmod lib/modules/3.4.35+/kernel/drivers/mmc/card/mmc_block.ko
+		insmod lib/modules/3.4.35+/kernel/drivers/mmc/host/ftsdc010.ko
+	fi	
+}
+test -e lib/modules/3.4.35+/kernel/drivers/input/touchscreen/cpe_ts/cpe_ts.ko && \
+{
+	insmod lib/modules/3.4.35+/kernel/drivers/input/touchscreen/cpe_ts/cpe_ts.ko
+	if [ "$auto" == "1" ]; then
+		rmmod cpe_ts
+		insmod lib/modules/3.4.35+/kernel/drivers/input/touchscreen/cpe_ts/cpe_ts.ko
+	fi	
+}
+test -e lib/modules/3.4.35+/kernel/drivers/video/FTLCDC100/faradayfb-main.ko && \
+{
+	insmod lib/modules/3.4.35+/kernel/drivers/video/cfbcopyarea.ko
+	insmod lib/modules/3.4.35+/kernel/drivers/video/cfbfillrect.ko
+	insmod lib/modules/3.4.35+/kernel/drivers/video/cfbimgblt.ko
+	insmod lib/modules/3.4.35+/kernel/drivers/video/FTLCDC100/faradayfb-main.ko
+}
+test -e lib/modules/3.4.35+/kernel/sound/nds32/snd-ftssp010.ko && \
+{	
+	insmod lib/modules/3.4.35+/kernel/sound/nds32/snd-ftssp010.ko
+	if [ "$auto" == "1" ]; then
+		rmmod snd_ftssp010
+		insmod lib/modules/3.4.35+/kernel/sound/nds32/snd-ftssp010.ko
+	fi	
+}
+test -e lib/modules/3.4.35+/kernel/drivers/watchdog/ftwdt010_wdt.ko && \
+{
+	insmod lib/modules/3.4.35+/kernel/drivers/watchdog/ftwdt010_wdt.ko
+	if [ "$auto" == "1" ]; then
+		rmmod ftwdt010_wdt
+		insmod lib/modules/3.4.35+/kernel/drivers/watchdog/ftwdt010_wdt.ko
+	fi	
+}
+test -e lib/modules/3.4.35+/kernel/drivers/rtc/rtc-ftrtc010.ko
+if [ "$?" == "0" -a "$auto" == "1" ]; then
+	insmod lib/modules/3.4.35+/kernel/drivers/rtc/rtc-ftrtc010.ko
+else
+	exit 1
+fi
+
+#for i in $drvs
+#do
+#drv=$(dmesg | grep "$i")
+#if [ "$?" == "1" ]; then
+#	echo "$i NOT exist"
+#	exit	
+#fi
+#done
+
+#exit
+
+udhcpc
+test -z $hostname1
+if [ "$?" == "1" ]; then
+	echo "mount hastname1 = $hostname1"
+	mount -t nfs -o nolock,rsize=1024,wsize=1024 $hostname1:$mnt_path1 mnt
+else
+	echo "mount hastname0 = $hostname0"
+	mount -t nfs -o nolock,rsize=1024,wsize=1024 $hostname0:$mnt_path0 mnt
+fi	
+
+if [ "$?" != "0" ]; then
+	echo "mount mnt fail"
+	exit 1
+fi
+lsmod 2>&1 | tee mnt/log/lsmod.log
+cp dmesg_boot.log mnt/log/
+mount /dev/mmcblk0p1 /tmp
+if [ "$?" != "0" ]; then
+	echo "mount tmp fail"
+	exit 1
+fi
+
+cd
+cd mnt
+dmesg > log/dmesg_run.log
+sh test_drivers.sh
