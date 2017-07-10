d497380a8a4ca20c302ab520ee3cfcdf5e3b28df
nylon <nylon7@andestech.com>
2017-07-10 15:48:06 +0800

    change driver test flow by module dependency file

--- disk/etc/init.d/run.sh
+++ disk/etc/init.d/run.sh
@@ -6,74 +6,20 @@ export mnt_path1=
 export auto=
 dmesg > dmesg_boot.log
 echo $(grep Machine /dmesg_boot.log| sed 's/^.*Andes //g') > platform.log
-drvs="FTGPIO010 faraday-rtc FTMAC100 ftsdc010 ADS7846 40x30 ftssp010"
 linux_ver=`uname -r`
-test -e lib/modules/$linux_ver/kernel/drivers/gpio/gpio-ftgpio010.ko && \
-{
-	insmod lib/modules/$linux_ver/kernel/drivers/gpio/gpio-ftgpio010.ko
-	if [ "$auto" == "1" ]; then
-		rmmod gpio-ftgpio010
-		insmod lib/modules/$linux_ver/kernel/drivers/gpio/gpio-ftgpio010.ko
-	fi
-}
-test -e lib/modules/$linux_ver/kernel/drivers/net/ethernet/faraday/ftmac100.ko && \
-{
-	insmod lib/modules/$linux_ver/kernel/drivers/net/ethernet/faraday/ftmac100.ko
-	if [ "$auto" == "1" ]; then
-		rmmod ftmac100
-		insmod lib/modules/$linux_ver/kernel/drivers/net/ethernet/faraday/ftmac100.ko
-	fi
-}
-test -e lib/modules/$linux_ver/kernel/drivers/mmc/host/ftsdc010.ko && \
-{
-	insmod lib/modules/$linux_ver/kernel/drivers/mmc/core/mmc_core.ko
-	insmod lib/modules/$linux_ver/kernel/drivers/mmc/card/mmc_block.ko
-	insmod lib/modules/$linux_ver/kernel/drivers/mmc/host/ftsdc010.ko
-	if [ "$auto" == "1" ]; then
-		rmmod ftsdc010
-		rmmod mmc_block
-		rmmod mmc_core
-		insmod lib/modules/$linux_ver/kernel/drivers/mmc/core/mmc_core.ko
-		insmod lib/modules/$linux_ver/kernel/drivers/mmc/card/mmc_block.ko
-		insmod lib/modules/$linux_ver/kernel/drivers/mmc/host/ftsdc010.ko
-	fi
-}
-test -e lib/modules/$linux_ver/kernel/drivers/input/touchscreen/cpe_ts/cpe_ts.ko && \
-{
-	insmod lib/modules/$linux_ver/kernel/drivers/input/touchscreen/cpe_ts/cpe_ts.ko
-	if [ "$auto" == "1" ]; then
-		rmmod cpe_ts
-		insmod lib/modules/$linux_ver/kernel/drivers/input/touchscreen/cpe_ts/cpe_ts.ko
-	fi
-}
-test -e lib/modules/$linux_ver/kernel/drivers/video/FTLCDC100/faradayfb-main.ko && \
-{
-	insmod lib/modules/$linux_ver/kernel/drivers/video/cfbcopyarea.ko
-	insmod lib/modules/$linux_ver/kernel/drivers/video/cfbfillrect.ko
-	insmod lib/modules/$linux_ver/kernel/drivers/video/cfbimgblt.ko
-	insmod lib/modules/$linux_ver/kernel/drivers/video/FTLCDC100/faradayfb-main.ko
-}
-test -e lib/modules/$linux_ver/kernel/sound/nds32/snd-ftssp010.ko && \
-{
-	insmod lib/modules/$linux_ver/kernel/sound/nds32/snd-ftssp010.ko
-	if [ "$auto" == "1" ]; then
-		rmmod snd_ftssp010
-		insmod lib/modules/$linux_ver/kernel/sound/nds32/snd-ftssp010.ko
-	fi
-}
-test -e lib/modules/$linux_ver/kernel/drivers/watchdog/ftwdt010_wdt.ko && \
-{
-	insmod lib/modules/$linux_ver/kernel/drivers/watchdog/ftwdt010_wdt.ko
-	if [ "$auto" == "1" ]; then
-		rmmod ftwdt010_wdt
-		insmod lib/modules/$linux_ver/kernel/drivers/watchdog/ftwdt010_wdt.ko
-	fi
-}
-test -e lib/modules/$linux_ver/kernel/drivers/rtc/rtc-ftrtc010.ko
-if [ "$?" == "0" -a "$auto" == "1" ]; then
-	insmod lib/modules/$linux_ver/kernel/drivers/rtc/rtc-ftrtc010.ko
-else
-	exit 1
+
+while read module
+do
+        if [ "$auto" == "1" ]; then
+            module_name=`basename $module .ko`
+            rmmod $module_name
+        fi
+        insmod /lib/modules/$linux_ver/$module
+
+done</lib/modules/$linux_ver/modules.order
+
+if [ "$auto" != "1" ]; then
+        exit 1
 fi
 
 #for i in $drvs
